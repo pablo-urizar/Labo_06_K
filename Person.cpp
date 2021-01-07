9ddb10ea8986a302b979158ec9e11f8b043470b5
Pablo <jose.urizar@heig-vd.ch>
2021-01-07 17:43:06 +0100

    SortBy

--- Person.cpp
+++ Person.cpp
@@ -2,9 +2,10 @@
 
 #include <utility>
 #include <iomanip>
+#include <algorithm>
 
 // DÉFINITION DES FONCTIONS MEMBRES
-// Getters
+//-----------------------------------------------------------------------------------------------------------//
 unsigned Person::getID() const {
     return this->id;
 }
@@ -26,6 +27,11 @@ string Person::getDate() const {
 unsigned Person::nextId = 0;
 unsigned Person:: nbre = 0;
 
+unsigned Person::nbrePerson() {
+    return nbre;
+};
+
+//-----------------------------------------------------------------------------------------------------------//
 // Constructeur par défaut
 Person::Person(string lastName, string firstName, string date) : lastName(lastName), firstName(firstName),
 date(date), id(++nextId) {++nbre;}
@@ -34,8 +40,11 @@ date(date), id(++nextId) {++nbre;}
 Person::Person(const Person& p) : lastName(p.lastName), firstName(p.firstName), date(p.date), id(p.id) {++nbre;}
 
 // Constructeur d'affectation
-void Person::operator=(const Person& p) {}
+//void Person::operator=(const Person& p) {}
 
+Person& Person::operator=(const Person& p) {}
+
+//-----------------------------------------------------------------------------------------------------------//
 ostream& operator<<(ostream& os, const Person& p) {
     os  << p.lastName
         << '\t' << p .firstName
@@ -43,4 +52,70 @@ ostream& operator<<(ostream& os, const Person& p) {
         << '\t' << "(id=" << to_string(p.id) << ')';
 
     return os;
+}
+//-----------------------------------------------------------------------------------------------------------//
+/*
+bool SortBy::sortLastName(const Person& p1, const Person& p2) {
+    return p1.getLastName() < p2.getLastName();
+}
+
+bool SortBy::sortFirstName(const Person& p1, const Person& p2) {
+    return p1.getFirstName() < p2.getFirstName();
+}
+
+bool SortBy::sortDate(const Person& p1, const Person& p2) {
+    return p1.getDate() < p2.getDate();
+}
+
+bool SortBy::sortId(const Person& p1, const Person& p2) {
+    return p1.getID() < p2.getID();
+}
+*/
+
+SortBy::SortBy(PERSON type) : type(type){}
+
+bool SortBy::operator() (const Person& p1, const Person& p2) {
+    switch (type) {
+        case PERSON::NO_ID:
+            return p1.getID() < p2.getID();
+
+        case PERSON::LASTNAME:
+            return (p1.getLastName() < p2.getLastName());
+
+        case PERSON::FIRSTNAME:
+            return (p1.getFirstName() < p2.getFirstName());
+
+        case PERSON::DATE:
+            return p1.getDate() < p2.getDate();
+
+    }
+}
+
+FindBy::FindBy(const PERSON &type, const string str) : type(type){
+    /*
+    PERSON type;
+    string findFirstName;
+    string findLastName;
+    string findDate;
+    unsigned findId;
+    */
+
+    switch (type) {
+        case PERSON::NO_ID:
+            break;
+
+        case PERSON::FIRSTNAME:
+            break;
+
+        case PERSON::LASTNAME:
+            break;
+
+        case PERSON::DATE:
+            break;
+    }
+
+}
+
+bool FindBy::operator()(Person p){
+
 }
