04ec64f6dc8db0979d958f8b58afb63d2343078e
Sar <sarah.jallon@gmail.com>
2021-01-07 19:46:38 +0100

    date header is done, a few of dateCPP functions implemented

--- Date.cpp
+++ Date.cpp
@@ -1,5 +1,62 @@
 #include "Date.h"
-#include <ostream>
+#include <iostream>
+
+//stream operator
+
+std::ostream& operator<<(std::ostream& os, const Date& date){
+   std::cout << date.getDay() << "-" << date.getMonthNo() << "-" << date.getYear();
+   return os;
+}
+
+//comparison operators
+bool operator<(const Date& lhs, const Date& rhs) {
+   return (lhs.getYear() < rhs.getYear())                                               ||
+          (lhs.getYear() == rhs.getYear()       && lhs.getMonthNo() < rhs.getMonthNo()) ||
+          (lhs.getMonthNo() == rhs.getMonthNo() && lhs.getDay() < rhs.getDay());
+}
+
+bool operator>(const Date& lhs, const Date& rhs) {
+   return rhs < lhs;
+}
+
+bool operator<=(const Date& lhs, const Date& rhs) {
+   return !(lhs > rhs);
+}
+
+bool operator>=(const Date& lhs, const Date& rhs) {
+   return !(lhs < rhs);
+}
+
+bool operator==(const Date& lhs, const Date& rhs) {
+   return lhs.getYear() == rhs.getYear()       &&
+          lhs.getMonthNo() == rhs.getMonthNo() &&
+          lhs.getDay() == rhs.getDay();
+}
+
+bool operator!=(const Date& lhs, const Date& rhs) {
+   return !(lhs == rhs);
+}
+
+//arithmetic operators
+
+Date operator+(Date lhs,int rhs){
+
+}
+
+Date operator-(Date lhs, int rhs){
+
+}
+
+//commutativity of the arithmetic operators
+
+Date operator+(int lhs,Date rhs){
+
+}
+Date operator-(int lhs, Date rhs){
+
+}
+
+//constructors
 
 Date::Date(){
    day = 1;
@@ -15,6 +72,7 @@ Date::Date(std::string string){
 
 }
 
+// getter and setters
 unsigned int Date::getDay() const {
    return day;
 }
@@ -23,7 +81,7 @@ void Date::setDay(unsigned int day) {
    Date::day = day;
 }
 
-unsigned int Date::getMonth() const {
+unsigned int Date::getMonthNo() const {
    return month;
 }
 
@@ -50,14 +108,71 @@ void Date::setCorrect(bool correct) {
    Date::correct = correct;
 }
 
-std::ostream& operator<<(std::ostream& os, const Date& date){
-   cout << date.getDay() << "-" << date.getMonth() << ""
+//compound assignement operators
+
+Date& Date::operator++(){
+
 }
 
- bool operator<(const Date& lhs, const Date& rhs) {/* comparaison < à écrire ici */}
- bool operator>(const Date& lhs, const Date& rhs) {return rhs < lhs;}
- bool operator<=(const Date& lhs, const Date& rhs) {return !(lhs > rhs);}
- bool operator>=(const Date& lhs, const Date& rhs) {return !(lhs < rhs);}
- bool operator==(const Date& lhs, const Date& rhs) {/* comparaison == à écrire ici */}
- bool operator!=(const Date& lhs, const Date& rhs) {return !(lhs == rhs);}
+Date& Date::operator++(int) {
 
+}
+
+Date& Date::operator--(){
+
+}
+
+Date& Date::operator--(int) {
+
+}
+
+Date& Date::operator+=(const int& rhs) {
+
+}
+
+Date& Date::operator-=(const int& rhs){
+
+}
+
+//assignement operator
+
+Date& Date::operator=(const Date&rhs){
+   this->setYear(rhs.getYear());
+   this->setMonth(rhs.getMonthNo());
+   this->setDay(rhs.getDay());
+}
+
+//string cast
+
+explicit operator std::string() const{
+
+}
+
+//validation functions
+bool Date::isValid(){
+
+}
+
+bool Date::isValid(unsigned day, unsigned month, unsigned year){
+
+}
+
+//leap year functions
+
+bool Date::isLeapYear(){
+   return (this->getYear() % 4 == 0 && this->getYear() % 100 != 0) || (this->getYear() % 400 == 0);
+}
+
+bool Date::isLeapYear(unsigned year){
+   return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+//number of day  in months functions
+
+unsigned Date::numberDaysInMonth(){
+   return this->getMonthNo() == int(Month::FEBRUARY) ? this->isLeapYear() ? 28 : 29 : 31 - this->getMonthNo() % 7 % 2;
+}
+
+static unsigned numberDaysInMonth(unsigned month, unsigned year){
+   return month == int(Month::FEBRUARY) ? Date::isLeapYear(year) ? 28 : 29 : 31 - month % 7 % 2;
+}
